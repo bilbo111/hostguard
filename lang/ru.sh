47d047048e6c25b725d808414c5cca7121176d28
bilbo111 <igornai52@gmail.com>
2026-07-16 15:27:25 +0900

    Create ru.sh

---
+++ lang/ru.sh
@@ -0,0 +1,215 @@
+#!/usr/bin/env bash
+
+###############################################################################
+# HostGuard
+# Russian language
+###############################################################################
+
+#
+# Common
+#
+
+TXT_YES="Да"
+TXT_NO="Нет"
+TXT_BACK="Назад"
+TXT_EXIT="Выход"
+TXT_CANCEL="Отмена"
+TXT_CONTINUE="Продолжить"
+
+#
+# Installer
+#
+
+TXT_WELCOME="Добро пожаловать в HostGuard"
+
+TXT_DESCRIPTION="HostGuard автоматически усиливает защиту сервера от злоупотреблений и устанавливает защитные правила межсетевого экрана."
+
+TXT_PRESS_ENTER="Нажмите Enter для продолжения..."
+
+TXT_LANGUAGE="Выберите язык"
+
+TXT_INSTALL="Установить HostGuard"
+
+TXT_UNINSTALL="Удалить HostGuard"
+
+TXT_UPDATE="Обновить базы"
+
+TXT_QUIT="Выход"
+
+#
+# System
+#
+
+TXT_SYSTEM="Информация о системе"
+
+TXT_OS="Операционная система"
+
+TXT_VERSION="Версия"
+
+TXT_ARCH="Архитектура"
+
+TXT_FIREWALL="Firewall"
+
+TXT_PACKAGE_MANAGER="Менеджер пакетов"
+
+TXT_INIT="Система инициализации"
+
+TXT_IPSET="Поддержка ipset"
+
+#
+# Providers
+#
+
+TXT_PROVIDER_SELECTION="Выберите списки блокировок"
+
+TXT_PROVIDER_SPAMHAUS="Spamhaus DROP"
+
+TXT_PROVIDER_EDROP="Spamhaus EDROP"
+
+TXT_PROVIDER_FIREHOL="FireHOL"
+
+TXT_PROVIDER_CUSTOM="Пользовательские списки"
+
+#
+# Firewall options
+#
+
+TXT_FIREWALL_OPTIONS="Настройки Firewall"
+
+TXT_ENABLE_SPAMHAUS="Использовать Spamhaus DROP"
+
+TXT_ENABLE_EDROP="Использовать Spamhaus EDROP"
+
+TXT_ENABLE_SSH_BLOCK="Блокировать исходящий SSH"
+
+TXT_ENABLE_SMTP_BLOCK="Блокировать исходящий SMTP"
+
+TXT_ENABLE_RDP_BLOCK="Блокировать исходящий RDP"
+
+TXT_ENABLE_LOGGING="Включить журналирование"
+
+TXT_ENABLE_AUTORELOAD="Автоматически обновлять базы"
+
+#
+# Installation
+#
+
+TXT_START_INSTALL="Начать установку"
+
+TXT_INSTALLING="Выполняется установка..."
+
+TXT_CREATING_IPSET="Создание ipset..."
+
+TXT_DOWNLOADING_DATABASES="Загрузка баз..."
+
+TXT_INSTALLING_RULES="Установка правил..."
+
+TXT_SAVING_RULES="Сохранение правил..."
+
+TXT_CREATING_TIMER="Создание автообновления..."
+
+TXT_FINISHED="Установка завершена."
+
+TXT_REBOOT_NOT_REQUIRED="Перезагрузка не требуется."
+
+#
+# Errors
+#
+
+TXT_ERROR="Ошибка"
+
+TXT_WARNING="Предупреждение"
+
+TXT_SUCCESS="Успешно"
+
+TXT_FAILED="Не удалось выполнить операцию."
+
+TXT_UNSUPPORTED_OS="Данная операционная система не поддерживается."
+
+TXT_NO_INTERNET="Нет подключения к Интернету."
+
+TXT_NO_ROOT="HostGuard должен быть запущен от root."
+
+TXT_FIREWALL_NOT_FOUND="Не удалось определить firewall."
+
+#
+# Uninstall
+#
+
+TXT_UNINSTALL_TITLE="Удаление HostGuard"
+
+TXT_UNINSTALL_CONFIRM="Удалить HostGuard с сервера?"
+
+TXT_REMOVING_RULES="Удаление правил..."
+
+TXT_REMOVING_IPSET="Удаление ipset..."
+
+TXT_REMOVING_TIMER="Удаление автообновления..."
+
+TXT_UNINSTALL_DONE="HostGuard успешно удалён."
+
+#
+# Update
+#
+
+TXT_UPDATE_TITLE="Обновление баз"
+
+TXT_UPDATING_DATABASES="Обновление списков..."
+
+TXT_UPDATE_DONE="Базы успешно обновлены."
+
+#
+# Menu
+#
+
+TXT_MENU_TITLE="Главное меню"
+
+TXT_MENU_INSTALL="Установить"
+
+TXT_MENU_UPDATE="Обновить базы"
+
+TXT_MENU_STATUS="Статус"
+
+TXT_MENU_UNINSTALL="Удалить"
+
+TXT_MENU_EXIT="Выход"
+
+#
+# Confirmation
+#
+
+TXT_CONFIRM_INSTALL="Продолжить установку?"
+
+TXT_CONFIRM_EXIT="Выйти из программы?"
+
+TXT_CONFIRM_UNINSTALL="Вы действительно хотите удалить HostGuard?"
+
+#
+# Summary
+#
+
+TXT_SUMMARY="Итоговая конфигурация"
+
+TXT_SUMMARY_PROVIDER="Провайдеры"
+
+TXT_SUMMARY_FIREWALL="Firewall"
+
+TXT_SUMMARY_AUTORELOAD="Автообновление"
+
+TXT_SUMMARY_SSH="Блокировка SSH"
+
+TXT_SUMMARY_SMTP="Блокировка SMTP"
+
+TXT_SUMMARY_RDP="Блокировка RDP"
+
+TXT_SUMMARY_LOGGING="Журналирование"
+
+TXT_READY="Готово к установке."
+
+#
+# Footer
+#
+
+TXT_THANKS="Спасибо за использование HostGuard."
+
+TXT_PROJECT="Open Source проект под лицензией GPL-3.0."
