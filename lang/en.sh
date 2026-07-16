8ebe372196180a0e9d9d04d83d5b96f147436c56
bilbo111 <igornai52@gmail.com>
2026-07-16 14:21:51 +0900

    Create en.sh

---
+++ lang/en.sh
@@ -0,0 +1,25 @@
+TITLE="HostGuard Installer"
+
+WELCOME="Welcome to HostGuard"
+
+MENU_INSTALL="Install HostGuard"
+
+MENU_EXIT="Exit"
+
+MENU_UNINSTALL="Uninstall"
+
+MENU_LANGUAGE="Language"
+
+ASK_SPAMHAUS="Install Spamhaus?"
+
+ASK_EDROP="Install Extended DROP?"
+
+ASK_SSH="Block outgoing SSH?"
+
+ASK_SMTP="Block outgoing SMTP?"
+
+ASK_RDP="Block outgoing RDP?"
+
+ASK_AUTOUPDATE="Enable automatic updates?"
+
+FINISHED="Installation completed."
