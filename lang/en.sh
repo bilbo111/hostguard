bbd33de1565ee57609f52347b5aff36c970da3ee
bilbo111 <igornai52@gmail.com>
2026-07-16 15:30:57 +0900

    Create en.sh

---
+++ lang/en.sh
@@ -0,0 +1,186 @@
+#!/usr/bin/env bash
+
+#########################################
+# HostGuard Language Pack
+# English
+#########################################
+
+LANG_NAME="English"
+
+# ----------------------------------------------------
+# General
+# ----------------------------------------------------
+
+TXT_WELCOME="Welcome to HostGuard"
+
+TXT_VERSION="Version"
+
+TXT_PRESS_ENTER="Press Enter to continue..."
+
+TXT_DONE="Done."
+
+TXT_ERROR="Error"
+
+TXT_WARNING="Warning"
+
+TXT_SUCCESS="Success"
+
+TXT_FAILED="Failed"
+
+TXT_YES="Yes"
+
+TXT_NO="No"
+
+TXT_INSTALL="Install"
+
+TXT_UNINSTALL="Uninstall"
+
+TXT_EXIT="Exit"
+
+TXT_BACK="Back"
+
+TXT_NEXT="Next"
+
+TXT_ENABLED="Enabled"
+
+TXT_DISABLED="Disabled"
+
+# ----------------------------------------------------
+# System checks
+# ----------------------------------------------------
+
+TXT_ROOT_REQUIRED="HostGuard must be run as root."
+
+TXT_UNSUPPORTED_OS="This operating system is not supported."
+
+TXT_DEBIAN_DETECTED="Debian detected"
+
+TXT_UBUNTU_DETECTED="Ubuntu detected"
+
+TXT_IPTABLES_DETECTED="iptables detected"
+
+TXT_NFTABLES_DETECTED="nftables detected"
+
+# ----------------------------------------------------
+# Main menu
+# ----------------------------------------------------
+
+TXT_MENU_TITLE="Main Menu"
+
+TXT_MENU_INSTALL="Install HostGuard"
+
+TXT_MENU_UNINSTALL="Remove HostGuard"
+
+TXT_MENU_EXIT="Exit"
+
+# ----------------------------------------------------
+# Providers
+# ----------------------------------------------------
+
+TXT_PROVIDER_TITLE="Select blocklist providers"
+
+TXT_PROVIDER_SPAMHAUS="Spamhaus DROP"
+
+TXT_PROVIDER_EDROP="Spamhaus EDROP"
+
+TXT_PROVIDER_FIREHOL="FireHOL (coming soon)"
+
+TXT_PROVIDER_CUSTOM="Custom Lists (coming soon)"
+
+TXT_PROVIDER_SELECTED="Selected"
+
+# ----------------------------------------------------
+# Firewall
+# ----------------------------------------------------
+
+TXT_FW_TITLE="Firewall configuration"
+
+TXT_FW_SSH="Block outbound SSH (22)"
+
+TXT_FW_SMTP="Block SMTP (25,465,587)"
+
+TXT_FW_RDP="Block RDP (3389)"
+
+TXT_FW_SAVE="Save firewall rules"
+
+TXT_FW_UPDATE="Enable automatic updates"
+
+TXT_FW_IPV6="Apply IPv6 rules"
+
+# ----------------------------------------------------
+# Installation
+# ----------------------------------------------------
+
+TXT_INSTALL_START="Starting installation..."
+
+TXT_INSTALL_PACKAGES="Installing required packages..."
+
+TXT_INSTALL_PROVIDER="Downloading blocklists..."
+
+TXT_INSTALL_FIREWALL="Configuring firewall..."
+
+TXT_INSTALL_SYSTEMD="Installing update service..."
+
+TXT_INSTALL_CONFIG="Creating configuration..."
+
+TXT_INSTALL_COMPLETE="Installation completed successfully."
+
+# ----------------------------------------------------
+# Uninstall
+# ----------------------------------------------------
+
+TXT_REMOVE_START="Removing HostGuard..."
+
+TXT_REMOVE_RULES="Removing firewall rules..."
+
+TXT_REMOVE_CONFIG="Removing configuration..."
+
+TXT_REMOVE_SYSTEMD="Removing services..."
+
+TXT_REMOVE_COMPLETE="HostGuard has been removed."
+
+# ----------------------------------------------------
+# Update
+# ----------------------------------------------------
+
+TXT_UPDATE_TITLE="Updating blocklists"
+
+TXT_UPDATE_START="Updating..."
+
+TXT_UPDATE_DOWNLOAD="Downloading latest lists..."
+
+TXT_UPDATE_APPLY="Applying updates..."
+
+TXT_UPDATE_DONE="Blocklists updated successfully."
+
+TXT_UPDATE_FAILED="Update failed."
+
+# ----------------------------------------------------
+# Automatic updates
+# ----------------------------------------------------
+
+TXT_AUTOUPDATE_ENABLE="Enable automatic updates"
+
+TXT_AUTOUPDATE_DISABLE="Disable automatic updates"
+
+TXT_AUTOUPDATE_INTERVAL="Update interval"
+
+# ----------------------------------------------------
+# Confirmation
+# ----------------------------------------------------
+
+TXT_CONFIRM_INSTALL="Start installation?"
+
+TXT_CONFIRM_UNINSTALL="Remove HostGuard?"
+
+TXT_CONFIRM_EXIT="Exit?"
+
+# ----------------------------------------------------
+# Final
+# ----------------------------------------------------
+
+TXT_FINISHED="Operation completed."
+
+TXT_REBOOT_RECOMMENDED="A system reboot is recommended."
+
+TXT_THANKS="Thank you for using HostGuard."
